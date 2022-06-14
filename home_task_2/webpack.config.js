4c347cd3ca892e31573fa12c8f35da9737253b26
galbeiroc <alcresgu_1989@hotmail.com>
2022-06-14 11:25:56 -0500

    move devServer config

--- home_task_2/webpack.config.js
+++ home_task_2/webpack.config.js
@@ -14,16 +14,6 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist')
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true
-  },
   module: {
     rules: [
       {
--- home_task_2/webpack.dev.js
+++ home_task_2/webpack.dev.js
@@ -1,9 +1,20 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const { EnvironmentPlugin } = require('webpack');
 const configWebpack = require('./webpack.config')
 
 const devConfig = {
   mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  },
   plugins: [
     new EnvironmentPlugin({
       API: 'htpps://dev.galbeiroc.com'
